3e92628db4a4f4021bdf71da1daad4e6a3e4b0ab
Sauron <sauron@Saurons-iMac.local>
2017-02-13 18:13:08 +0100

    initial commit

---
+++ ttcnCodeGenerator/SIPfunctions.swift
@@ -0,0 +1,17 @@
+//
+//  SIPfunctions.swift
+//  ttcnCodeGenerator
+//
+//  Created by Sauron on 2017. 02. 13..
+//  Copyright © 2017. Sauron. All rights reserved.
+//
+
+import Foundation
+
+enum SIPFunctions: String {
+    case register = "REGISTER"
+    case invite = "INVITE"
+    case ack = "ACK"
+    case ok = "200OK"
+    case bye = "BYE"
+}
--- ttcnCodeGenerator/callFlowViewController.swift
+++ ttcnCodeGenerator/callFlowViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  callFlowViewController.swift
 //  ttcnCodeGenerator
 //
 //  Created by Sauron on 2017. 02. 13..
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class callFlowViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendSip(_ sender: UIButton) {
+        
+    }
 
 }
 
