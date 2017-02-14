dd25c5c8d307b02b4efeb9ec8c35aca373c0e15f
Sauron <sauron@Saurons-iMac.local>
2017-02-14 16:04:40 +0100

    swipe INVITE works

--- ttcnCodeGenerator/callFlowViewController.swift
+++ ttcnCodeGenerator/callFlowViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class callFlowViewController: UIViewController {
 
+    @IBOutlet weak var userA: UIButton!
+    @IBOutlet weak var originatingMtas: UIButton!
+    @IBOutlet weak var HSSNode: UIButton!
+    @IBOutlet weak var userB: UIButton!
+    @IBOutlet weak var CDFNode: UIButton!
+    
+    @IBOutlet weak var labelOne: UILabel!
+    
+    @IBOutlet weak var labelTwo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        labelOne.isHidden = true
+        labelTwo.isHidden = true
+        originatingMtas.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,7 +32,18 @@ class callFlowViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func sendSip(_ sender: UIButton) {
+    @IBAction func sipMessage(_ sender: UISwipeGestureRecognizer) {
+        if (sender.direction == .right) {
+            labelOne.text = "INVITE"
+            labelOne.isHidden = false
+            originatingMtas.isHidden = false
+        } else if (sender.direction == .left) {
+            labelTwo.text = "NO RECIPIENT!"
+            labelTwo.isHidden = false
+        }
+    }
+    
+    @IBAction func mtasMessages(_ sender: UISwipeGestureRecognizer) {
         
     }
 
